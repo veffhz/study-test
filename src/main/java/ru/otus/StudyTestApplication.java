0801c0086619d693640fd7f0d5bb0bd83817d98a
V.Danilyuk <veffhz@gmail.com>
2019-03-15 12:15:15 +0300

    add main class. banner.

---
+++ src/main/java/ru/otus/StudyTestApplication.java
@@ -0,0 +1,30 @@
+package ru.otus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import ru.otus.service.TestService;
+
+@SpringBootApplication
+public class StudyTestApplication {
+
+	@Bean
+	public MessageSource messageSource() {
+		ReloadableResourceBundleMessageSource ms = new ReloadableResourceBundleMessageSource();
+		ms.setBasename("/i18n/bundle");
+		ms.setDefaultEncoding("UTF-8");
+		return ms;
+	}
+
+	public static void main(String[] args) {
+        ConfigurableApplicationContext applicationContext =
+                SpringApplication.run(StudyTestApplication.class, args);
+
+        TestService testService = applicationContext.getBean(TestService.class);
+        testService.runTest();
+    }
+
+}
